3eb95cbe87edade21492527d5f12cfb425aaafa9
Michael Clarke <michael.m.clarke@gmail.com>
2016-07-25 14:39:57 -0700

    Add option to configure the header name the username is passed under

--- README.md
+++ README.md
@@ -10,11 +10,38 @@ To install this module in II7 or above:
 1. Copy UsernamePassingModule.dll from output directory (debug/bin or release/bin) to the bin directory of the IIS site you're wanting to pass a username from
 2. Add a Web.config, or update your existing Web.config in the root of your site, so it contains a system.WebServer element, containing a modules element, with an add entry for the UsernamePassingModule, similar to:
 ~~~
-  <configuration>
-	  <system.webServer>
-		  <modules>
-			  <add name="Username" type="UsernamePassingModule.UsernamePassingModule"/>
-		  </modules>
-	  </system.webServer>
-  </configuration>
+   <configuration>
+      <system.webServer>
+         <modules>
+            <add name="Username" type="UsernamePassingModule.UsernamePassingModule"/>
+         </modules>
+      </system.webServer>
+   </configuration>
+~~~
+
+## Advanced Configuration
+It is possible to pass additional parameters to this module using a custom config setion:
+1. If your Web.xml does not already contain a 'configSections' element then add one in the root 'configuration' element of your Web.xml
+2. Add a 'section' element to this 'configSections' with an element named 'name' with the value of 'usernamePassingModule' and a second attributed named 'type' and with a value of 'System.Configuration.NameValueSectionHandler'
+3. Add a 'usernamePassingModule' element to the root 'configuration' element
+4. Add an 'add' element as a child of the 'usernamePaddingModule' element for each configuration element you want to pass, e.g. to configure the name used in the HTTP header add an 'add' element with a paremeter named 'key' and with the value of 'headerName', and a second parametere with the name 'value' and the value set to the name you want the header sent under. This value must be valid for an HTTP header name: containing only Alphanumeric characters, potentially seperated by hyphens.
+
+To setup the module to pass the username header under the name 'Authenticated-User', you Web.config needs to look similar to following:
+~~~
+   <configuration>
+      <configSections>
+         <section name="usernamePassingModule" type="System.Configuration.NameValueSectionHandler" />
+      </configSections>
+
+      <system.webServer>
+         <modules>
+            <add name="Username" type="UsernamePassingModule.UsernamePassingModule"/>
+         </modules>
+      </system.webServer>
+
+      <usernamePassingModule>
+         <add key="headerName" value="Authenticated-User"/>
+      </usernamePassingModule>
+
+   </configuration>
 ~~~
--- UsernamePassingModule/UsernamePassingModule.cs
+++ UsernamePassingModule/UsernamePassingModule.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Specialized;
+using System.Configuration;
 using System.Security.Principal;
 using System.Web;
 
@@ -7,20 +9,38 @@ namespace UsernamePassingModule
 /// <summary>
 /// <para>IIS contains a URLRewirite module which allows requests to be modified before being passed to the final processing endpoint, such as adding parameters or headers to the request.
 /// However, the rewrite content and target is created before the Authenticate step in the request pipeline, meaning any details obtained during authentication - such as the user's
-/// username - cannot be passed to the target of the rewrite. This module runs between the authenticate step and the execution of the rewrite, adding an 'X-REMOTE-USER' header to the
+/// username - cannot be passed to the target of the rewrite. This module runs between the authenticate step and the execution of the rewrite, adding an HTTP header to the
 /// rewritten request content, containing the principal name of the currently authenticated user (or null if no user is authenticated). This allows any downstream server to retrieve
 /// the currently authenticated user from the request by reviewing the request headers, without having to understand Kerberos, NTLM or some other authentication mechanism details that
-/// may be held in the forwarded request.</para>
+/// may be held in the forwarded request. The name of the request header is configurable in Web.config (or App.config) under the property 'headerName' within the 'usernamePassingModule',
+/// with the name defaulting to 'X-Remote-User' if the configuration doesn't exist or is invalid.</para>
 /// 
 /// <para>WARNING: This module does not provide any way for the downstream server to identify this header has originated from this module: someone who can run direct requests to the backend
-/// server could inject their own HTTP 'X-Remote-User' header thereby spoofing an authenticated login. It is therefore adivseable that anyone using this module ensure that communication
-/// with a backend server is restricted so the only source location is any IIS server running as a proxy with this module enabled.</para>
+/// server could inject their own HTTP header matching the name being passed by this module thereby spoofing an authenticated login. It is therefore adivseable that anyone using this module
+/// ensure that communication with a backend server is restricted so the only source location is any IIS server running as a proxy with this module enabled.</para>
 /// </summary>
     public class UsernamePassingModule : IHttpModule
     {
 
+        private string headerName = "X-Remote-User";
+
         public void Init(HttpApplication context)
         {
+            // check Web.config (or App.config if you've managed to run this as a Standalone App!) for a section within the 'config' XML section named 'UsernamePassingModule'
+            object configurationSection = ConfigurationManager.GetSection("usernamePassingModule");
+
+            // check if the section exists, and is a NameValueCollection (users can configure it as something else, but we don't support that)
+            if (null != configurationSection && configurationSection is NameValueCollection) {
+
+                // retrieve the value of the property named 'headerName' from the configuration section
+                string potentialHeaderName = ((NameValueCollection) configurationSection)["headerName"];
+
+                // check the property value exists and isn't empty, then set it to be used as the header name in all forwarded requests
+                if (!string.IsNullOrEmpty(potentialHeaderName)) {
+                    headerName = potentialHeaderName;
+                }
+            }
+
             // add a call to ProcessRequest into the pipeline for each HTTP request after the authentication setp
             context.PostAuthenticateRequest += ProcessRequest;
         }
@@ -46,8 +66,9 @@ namespace UsernamePassingModule
                 username = user.Identity.Name;
             }
             
-            // add the username (possibly null if user is unauthenticated) to the request under the 'X-Remote-User' header
-            context.Request.Headers.Add("X-Remote-User", username);
+            // add the username (possibly null if user is unauthenticated) to the request under the currently configured headername:
+            // either set by the user in Web.config, or defaulting to X-Remote-User if the settings are invalid or undefined
+            context.Request.Headers.Add(headerName, username);
         }
 
 
